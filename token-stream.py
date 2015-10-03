0b97d79c2952a897e814c6534a8a72e7dfe6a04b
chchiou <chchiou@users.noreply.github.com>
2015-10-03 19:58:19 +0200

    Update token-stream.py
    Modified to the one able to be executed on my laptop

--- token-stream.py
+++ token-stream.py
@@ -1,20 +1,38 @@
-# -*- coding: utf_8 -*-
-
-import tweepy, re
+import tweepy
+import json
+import re
 
 import sqlite3
 sql_conn = sqlite3.connect('tweets.db', isolation_level=None )
 sql_cur = sql_conn.cursor()
 
-## API KEY SETTINGS
-auth1 = tweepy.auth.OAuthHandler('YOUR KEY','YOUR KEY')
-auth1.set_access_token('YOUR KEY','YOUR KEY')
-############
 
-api = tweepy.API(auth1)
-
+# Authentication details. To  obtain these visit dev.twitter.com
+consumer_key = 'IsLPAN0ECZUFgVpQ1hgDAeAHk'
+consumer_secret = 'retGBqwK9CdGlzKovsTiDq5MCWGUnDEQ6thDVtCSFL1fDPcHc2'
+access_token = '356253611-JZaxzUvLSUzQTnVaO2YTsgFvFuVAMXB46ONQwPna'
+access_token_secret = 'Qko54nhzdQinOd3MZNAgJMM7sqlIy0oWU9f6f5EQPDpZw'
 
 nice_token = re.compile(r"^\w+[\w':;.,?!]*$", flags=re.UNICODE)
+
+# This is the listener, resposible for receiving data
+class StdOutListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(ascii(status.text))
+        tokenizza(status.text)
+'''        
+    def on_data(self, data):
+        # Twitter returns data in JSON format - we need to decode it first
+        decoded = json.loads(data)
+
+        # Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
+        print( '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore')))
+        print('')
+        return True
+
+    def on_error(self, status):
+        print(status)
+'''
 
 def test(token):
 	return nice_token.match(token) is not None and token != u'rt'
@@ -55,9 +73,9 @@ def tokenizza(testo):
 			elif i == len(token_list) -1 and multigram[-1] != '^':
 				multigram.append('^')
 
-	print ';;;;;;;;;;;;;'
-	print multigram
-	print ';;;;;;;;;;;;;'
+	print(';;;;;;;;;;;;;')
+	print(multigram)
+	print(';;;;;;;;;;;;;')
 
 	cache = []
 	result = []
@@ -80,23 +98,16 @@ def tokenizza(testo):
 	sql_cur.executemany('INSERT INTO multigrammi VALUES (?, ?, ?, ?, ?, ?)', result)
 
 
-class StreamListener(tweepy.StreamListener):
-	def on_status(self, status):
-		print status.text
-		print r"\\\\\\ "
-		tokenizza(status.text)
-		print r'////// '
-
-def main():
-	l = StreamListener()
-	streamer = tweepy.Stream(auth=auth1, listener=l, timeout=3000000000 )
-	streamer.sample()
-
-
 if __name__ == '__main__':
-	try:
-		main()
-	except (KeyboardInterrupt, SystemExit):
-		sql_cur.close()
-		sql_conn.close()
-		print '\nOK, bye'
+    l = StdOutListener()
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    print("Showing all new tweets for #programming:")
+
+    # There are different kinds of streams: public stream, user stream, multi-user streams
+    # In this example follow #programming tag
+    # For more details refer to https://dev.twitter.com/docs/streaming-apis
+    stream = tweepy.Stream(auth, l)
+    #stream.filter(stream.filter(track=['#mlb']))
+    stream.sample()
